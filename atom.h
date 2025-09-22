6017e3af30178e9eff79aae26e9f11a16c7e25f5
aautret <aautret@student.42.fr>
2025-09-22 18:50:39 +0200

    debut tokenisation

--- atom.h
+++ atom.h
@@ -6,26 +6,33 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:55:24 by aautret           #+#    #+#             */
-/*   Updated: 2025/09/22 17:15:44 by aautret          ###   ########.fr       */
+/*   Updated: 2025/09/22 18:04:25 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ATOM_H
 # define ATOM_H
 
-#include "libft/libft.h"
-#include <stdlib.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <unistd.h>
-#include <stddef.h>
+# include "libft/libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stddef.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 // #include <string.h>
 // #include <fcntl.h>
 // #include <limits>
 
+typedef struct s_token
+{
+	char			*value; // le texte du token
+	int				type;    // 0 = mot, 1 = opérateur, etc.
+	struct s_token	*next;
+}					t_token;
 
-int	valide_quote(char *str);
+int					valide_quote(char *str);
+char				*only_one_space(char *str);
 
 
 #endif
--- src/parsing.c
+++ src/parsing.c
@@ -6,7 +6,7 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:39:47 by aautret           #+#    #+#             */
-/*   Updated: 2025/09/22 17:18:01 by aautret          ###   ########.fr       */
+/*   Updated: 2025/09/22 18:50:11 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,30 @@ int	valide_quote(char *str)
 	if (flag_open % 2 != 0)
 		return (1);
 	return (0);
+}
+
+
+void	tokenizer(t_token *token, char *str)
+{
+	int		i;
+	int		j;
+	char	*res;
+
+	i = 0;
+	j = 0;
+	res = malloc(ft_strlen(str));
+	while (str[i])
+	{
+		if (str[i] == 32)
+			i++;
+		else
+		{
+			res[j] = str[i];
+			i++;
+			j++;
+		}
+	}
+	token->value = res;
+	//type a checker
+	token = token->next;
 }
