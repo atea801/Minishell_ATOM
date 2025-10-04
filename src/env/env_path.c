42a31b24ed4dcfd59dcbf8b4873138f7e9d20d35
TOM <tomlorette2703@gmail.com>
2025-10-04 15:51:20 +0200

    ENV : minimal env

---
+++ src/env/env_path.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_path.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/04 13:51:19 by tlorette          #+#    #+#             */
+/*   Updated: 2025/10/04 15:07:42 by tlorette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "atom.h"
+
+void	create_minimal_env(t_atom_env **env)
+{
+	char	*cwd;
+	char	*username;
+	char	*path_home;
+
+	cwd = getcwd(NULL, 0);
+	username = get_username(cwd);
+	path_home = get_home(cwd);
+	add_node_to_end(env, "USER", username);
+	add_node_to_end(env, "HOME", path_home);
+	add_node_to_end(env, "PATH", "/usr/bin:/bin:/usr/local/bin");
+	add_node_to_end(env, "OLDPWD", "");
+	add_node_to_end(env, "PWD", cwd);
+	free(cwd);
+	free(username);
+	free(path_home);
+}
+
+char	*get_home(char *cwd)
+{
+	int		i;
+	int		count_slash;
+
+	count_slash = 0;
+	i = 0;
+	while (cwd[i])
+	{
+		if (cwd[i] == '/')
+			count_slash++;
+		if (count_slash == 3)
+			return (ft_substr(cwd, 0, i));
+		i++;
+	}
+	return (NULL);
+}
+
+char	*get_username(char *cwd)
+{
+	int	i;
+
+	i = 6;
+	while (cwd[i])
+	{
+		if (cwd[i] == '/')
+			return (ft_substr(cwd, 6, i - 6));
+		i++;
+	}
+	return (NULL);
+}
+
+
