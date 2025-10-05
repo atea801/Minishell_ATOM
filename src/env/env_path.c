3868ef06ebefddcdcd39b8711d27573b3c406773
TOM <tomlorette2703@gmail.com>
2025-10-05 11:25:32 +0200

    ENV_MIN : modifications des cle necessaire dans lenv min

--- src/env/env_path.c
+++ src/env/env_path.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env_path.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/04 13:51:19 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/04 16:10:46 by aautret          ###   ########.fr       */
+/*   Updated: 2025/10/05 11:17:20 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,38 +14,43 @@
 
 /**
  * @brief Creation mini environnement impermeable au -i env
- * 
- * @param env 
+ *
+ * @param env
  */
 void	create_minimal_env(t_atom_env **env)
 {
 	char	*cwd;
-	char	*username;
-	char	*path_home;
+	char	*minishell_path;
 
+	// char	*username;
+	// char	*path_home;
 	cwd = getcwd(NULL, 0);
-	username = get_username(cwd);
-	path_home = get_home(cwd);
-	add_node_to_end(env, "USER", username);
-	add_node_to_end(env, "HOME", path_home);
-	add_node_to_end(env, "PATH", "/usr/bin:/bin:/usr/local/bin");
-	add_node_to_end(env, "OLDPWD", "");
+	minishell_path = ft_strjoin(cwd, "/minishell");
+
+	// username = get_username(cwd);
+	// path_home = get_home(cwd);
+	// add_node_to_end(env, "USER", username);
+	// add_node_to_end(env, "HOME", path_home);
 	add_node_to_end(env, "PWD", cwd);
+	add_node_to_end(env, "SHLVL", "1");
+	add_node_to_end(env, "PATH", "/usr/bin:/bin");
+	add_node_to_end(env, "_", minishell_path);
+	add_node_to_end(env, "OLDPWD", "");
 	free(cwd);
-	free(username);
-	free(path_home);
+	// 	free(username);
+	// 	free(path_home);
 }
 
 /**
  * @brief Permet de recuperer le path home
- * 
- * @param cwd 
+ *
+ * @param cwd
  * @return char* /home/username
  */
 char	*get_home(char *cwd)
 {
-	int		i;
-	int		count_slash;
+	int	i;
+	int	count_slash;
 
 	count_slash = 0;
 	i = 0;
@@ -62,8 +67,8 @@ char	*get_home(char *cwd)
 
 /**
  * @brief Permet de recuperer le username
- * 
- * @param cwd 
+ *
+ * @param cwd
  * @return char* username
  */
 char	*get_username(char *cwd)
@@ -79,5 +84,3 @@ char	*get_username(char *cwd)
 	}
 	return (NULL);
 }
-
-
