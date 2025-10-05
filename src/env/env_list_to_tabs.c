baf6dd712d066eee065f9fa6c988bfcb34235c56
aautret <aautret@student.42.fr>
2025-10-05 18:14:14 +0200

    ENV : creation fonciton qui permet de transformer la structure de l'env en tableau (char **)

---
+++ src/env/env_list_to_tabs.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   env_list_to_tabs.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/05 16:38:47 by aautret           #+#    #+#             */
+/*   Updated: 2025/10/05 18:05:34 by aautret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "atom.h"
+
+void	print_env_tab(char **tab_env)
+{
+	int	i;
+
+	i = 0;
+	if (!tab_env)
+		return ;
+	while (tab_env[i])
+	{
+		printf("tab_env[%d]: %s\n", i, tab_env[i]);
+		i++;
+	}
+}
+
+int	count_var_env(t_atom_env *env_list)
+{
+	int	count;
+
+	count = 0;
+	while (env_list)
+	{
+		count++;
+		env_list = env_list->next;
+	}
+	return (count);
+}
+
+char	**env_list_to_tab(t_atom_env *env_list)
+{
+	t_atom_env	*start;
+	char		**tab;
+	int			count;
+	int			key_len;
+	int			value_len;
+	int			i;
+	int			j;
+	int			k;
+	int			x;
+
+	start = env_list;
+	i = 0;
+	count = count_var_env(env_list);
+	// alloue le nombre de boite necessaire mais n'alloue pas le
+	// contenu de la boite
+	tab = malloc(sizeof(char *) * (count + 1));
+	if (!tab)
+		return (NULL);
+	
+	// allouer le contenu de la boite
+	env_list = start;
+	i = 0;
+	while (env_list && i < count)
+	{
+		if (env_list->key)
+			key_len = ft_strlen(env_list->key);
+		else
+			key_len = 0;
+		if (env_list->has_value && env_list->value)
+			value_len = ft_strlen(env_list->value);
+		else
+			value_len = 0;
+		tab[i] = malloc(sizeof(char) * (key_len + value_len + 2));
+		if (!tab[i])
+		{
+			// Libère la mémoire déjà allouée
+			x = 0;
+			while (x < i)
+			{
+				free(tab[x]);
+				x++;
+			}
+			free(tab);
+			return (NULL);
+		}
+		i++;
+		env_list = env_list->next;
+	}
+	// remplir le contenu de la boite
+	env_list = start;
+	i = 0;
+	while (env_list && i < count)
+	{
+		j = 0;
+		if (env_list->key)
+		{
+			while (env_list->key[j])
+			{
+				tab[i][j] = env_list->key[j];
+				j++;
+			}
+		}
+		tab[i][j++] = '=';
+		if (env_list->has_value && env_list->value)
+		{
+			k = 0;
+			while (env_list->value[k])
+			{
+				tab[i][j++] = env_list->value[k++];
+			}
+		}
+		tab[i][j] = '\0';
+		i++;
+		env_list = env_list->next;
+	}
+	tab[i] = NULL;
+	return (tab);
+}
