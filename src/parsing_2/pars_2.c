28518544d31a4edb97d2795ec4a0e1330d2d0a78
aautret <aautret@student.42.fr>
2025-10-05 15:51:06 +0200

    PARS 2 : ajout parsing_2 fonction principale + fonction count cmd

--- src/parsing_2/pars_2.c
+++ src/parsing_2/pars_2.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pars_2_utils.c                                     :+:      :+:    :+:   */
+/*   pars_2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/05 12:42:32 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/05 14:54:55 by aautret          ###   ########.fr       */
+/*   Created: 2025/10/05 14:55:34 by aautret           #+#    #+#             */
+/*   Updated: 2025/10/05 14:59:44 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
 
-int	count_cmd(t_token *token_head)
+void	parsing_2(t_token *token_head)
 {
-	int	count;
-
-	count = 0;
-	while(token_head)
-	{
-		if (token_head->type)
-			count++;
-		token_head = token_head->next;
-	}
-	return (count);
+	printf("nbr = %d\n", count_cmd(token_head));
+	return ;
 }
