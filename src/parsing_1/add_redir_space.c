82df19e17e3e14f977f8d45dec767e2817410008
TOM <tomlorette2703@gmail.com>
2025-10-07 15:40:17 +0200

    TOKEN_1 : gestion des chevrons dans token 1

--- src/parsing_1/add_redir_space.c
+++ src/parsing_1/add_redir_space.c
@@ -6,11 +6,28 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 14:30:07 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/05 13:42:38 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/07 15:19:24 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
+
+int	count_double(char *input)
+{
+	int	count;
+	int	i;
+
+	i = 0;
+	count = 0;
+	while (input[i])
+	{
+		if ((input[i] == '>' && input[i + 1] == '>') || (input[i] == '<'
+				&& input[i + 1] == '<'))
+			count++;
+		i++;
+	}
+	return (count);
+}
 
 int	count_redir(char *input)
 {
@@ -28,26 +45,85 @@ int	count_redir(char *input)
 	return (count);
 }
 
+// char	*add_redir_space(char *input)
+// {
+// 	char	*res;
+// 	int		count;
+// 	int		i;
+// 	int		j;
+
+// 	count = count_redir(input);
+// 	res = malloc(sizeof(char) * (ft_strlen(input) + count * 2 + 1));
+// 	if (!res)
+// 		return (NULL);
+// 	i = 0;
+// 	j = 0;
+// 	while (input[i])
+// 	{
+// 		if ((input[i] == '>' && input[i + 1] == '>') || (input[i] == '<'
+// 				&& input[i + 1] == '<'))
+// 		{
+// 			res[j++] = ' ';
+// 			res[j++] = input[i++];
+// 			res[j++] = input[i++];
+// 			res[j++] = ' ';
+// 		}
+// 		else if (input[i] == '>' || input[i] == '<')
+// 		{
+// 			res[j++] = ' ';
+// 			res[j++] = input[i++];
+// 			res[j++] = ' ';
+// 		}
+// 		else
+// 			res[j++] = input[i++];
+// 	}
+// 	res[j] = '\0';
+// 	return (res);
+// }
+
+int	is_redir(char c)
+{
+	return (c == '>' || c == '<');
+}
+
 char	*add_redir_space(char *input)
 {
+	int		i = 0, j;
 	char	*res;
-	int		count;
-	int		i;
-	int		j;
 
-	count = count_redir(input);
-	res = malloc(sizeof(char) * (ft_strlen(input) + count * 2 + 1));
+	i = 0, j = 0;
+	int len = ft_strlen(input) * 3 + 1; // Large, mais on réduit après si besoin
+	res = malloc(len);
 	if (!res)
 		return (NULL);
-	i = 0;
-	j = 0;
 	while (input[i])
 	{
-		if (input[i] == '>' || input[i] == '<')
+		// Détection séquence invalide
+		if ((input[i] == '>' || input[i] == '<')
+			&& ((input[i] == '>' && input[i + 1] == '>'
+					&& (input[i + 2] == '>')) || ((input[i] == '<')
+					&& input[i + 1] == '<' && input[i + 2] == '<')))
 		{
-			res[j++] = ' ';
+			free(res);
+			return (ft_strdup(input));
+		}
+		// Double redirection
+		if ((input[i] == '>' && input[i + 1] == '>') || (input[i] == '<'
+				&& input[i + 1] == '<'))
+		{
 			res[j++] = input[i++];
-			res[j++] = ' ';
+			res[j++] = input[i++];
+			// Ajoute espace si collé à un mot
+			if (input[i] && input[i] != ' ' && !is_redir(input[i]))
+				res[j++] = ' ';
+		}
+		// Simple redirection
+		else if (is_redir(input[i]))
+		{
+			res[j++] = input[i++];
+			// Ajoute espace si collé à un mot
+			if (input[i] && input[i] != ' ' && !is_redir(input[i]))
+				res[j++] = ' ';
 		}
 		else
 			res[j++] = input[i++];
