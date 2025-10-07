3294e01a6f91718d471bb92c7fa33a230a439567
TOM <tomlorette2703@gmail.com>
2025-10-07 15:41:32 +0200

    PARS 2 : suppression des printf a l'interieur (tt les printf sont dans le main)

--- src/parsing_2/pars_2.c
+++ src/parsing_2/pars_2.c
@@ -6,7 +6,7 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 14:55:34 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/06 18:33:07 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/07 13:51:38 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,5 @@ void	parsing_2(t_token *token_head, t_token_2 *token_2)
 	t_head_2 = token_2;
 	printf("nbr = %d\n", count_cmd(token_print));
 	print = get_input_pos(&token_head, &t_head_2);
-	print_token_2_list_type(print);
 	return ;
 }
