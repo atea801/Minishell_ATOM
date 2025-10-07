65ec4223c338d7573c08d83e1ba0a14dea5fd10a
TOM <tomlorette2703@gmail.com>
2025-10-07 17:18:21 +0200

    TOKEN__TYPE : minimisation de la fonction get_input_pos

--- src/tokenizer_2/token_2_type.c
+++ src/tokenizer_2/token_2_type.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   token_2_type.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 17:06:04 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/07 15:50:28 by aautret          ###   ########.fr       */
+/*   Updated: 2025/10/07 16:46:16 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 /**
  * @brief Alloue l'espace necessaire pour la structure token_2->next
- * 
- * @param token_2 
+ *
+ * @param token_2
  */
 void	put_token_2(t_token_2 **token_2)
 {
@@ -35,9 +35,9 @@ void	put_token_2(t_token_2 **token_2)
 
 /**
  * @brief Alloue la taille de l'input CMD
- * 
- * @param str 
- * @return char* 
+ *
+ * @param str
+ * @return char*
  */
 char	*malloc_cmd(char *str)
 {
@@ -53,9 +53,9 @@ char	*malloc_cmd(char *str)
 
 /**
  * @brief Copie et alloue le contenu de token (token->value)
- * 
- * @param str 
- * @return char* 
+ *
+ * @param str
+ * @return char*
  */
 char	*copy_token_value(char *str)
 {
@@ -86,26 +86,28 @@ t_token_2	*get_input_pos(t_token **token_1, t_token_2 **token_2)
 		return (NULL);
 	while (token_head_1 && token_head_1->type)
 	{
-		put_token_2(&token_head_2);
-		if (first_word == 0 && ft_strcmp(token_head_1->type, "MOT") == 0)
-		{
-			fill_cmd(&token_head_1, &token_head_2);
-			token_head_2 = token_head_2->next;
-			first_word = 1;
-		}
-		else if (first_word == 1 && ft_strcmp(token_head_1->type, "MOT") == 0)
-		{
-			fill_args(&token_head_1, &token_head_2);
-			token_head_2 = token_head_2->next;
-		}
-		else
-		{
-			fill_heredoc_append(&token_head_1, &token_head_2);
-			fill_redirin_redirout(&token_head_1, &token_head_2);
-			token_head_2 = token_head_2->next;
-			first_word = 0;
-		}
+		first_word = get_pos(token_head_1, token_head_2, first_word);
+		token_head_2 = token_head_2->next;
 		token_head_1 = token_head_1->next;
 	}
 	return (token_head_2);
+}
+
+int	get_pos(t_token *token_head_1, t_token_2 *token_head_2, int first_word)
+{
+	put_token_2(&token_head_2);
+	if (first_word == 0 && ft_strcmp(token_head_1->type, "MOT") == 0)
+	{
+		fill_cmd(&token_head_1, &token_head_2);
+		first_word = 1;
+	}
+	else if (first_word == 1 && ft_strcmp(token_head_1->type, "MOT") == 0)
+		fill_args(&token_head_1, &token_head_2);
+	else
+	{
+		fill_heredoc_append(&token_head_1, &token_head_2);
+		fill_redirin_redirout(&token_head_1, &token_head_2);
+		first_word = 0;
+	}
+	return (first_word);
 }
