e20c630a7a8ad846940f38cf23c53bb9f6555999
TOM <tomlorette2703@gmail.com>
2025-10-07 15:42:50 +0200

    TOKEN 2 ; fill token type

--- src/tokenizer_2/token_2_find_type.c
+++ src/tokenizer_2/token_2_find_type.c
@@ -6,38 +6,55 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 17:36:06 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/06 18:56:54 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/07 15:39:21 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
 
-void	fill_cmd_or_args(t_token **token_1, t_token_2 **token_2)
+
+void	fill_cmd(t_token **token_1, t_token_2 **token_2)
 {
 	t_token		*t_head_1;
 	t_token_2	*t_head_2;
-	t_token_2	*prev;
 
 	t_head_1 = *token_1;
 	t_head_2 = *token_2;
-	prev = NULL;
+	// if (!token_1)
+	// 	return ;
+	t_head_2->type = "CMD";
+	t_head_2->value = copy_token_value(t_head_1->value);
+}
+
+void	fill_args(t_token **token_1, t_token_2 **token_2)
+{
+	t_token		*t_head_1;
+	t_token_2	*t_head_2;
+
+	t_head_1 = *token_1;
+	t_head_2 = *token_2;
+	t_head_2->type = "ARGS";
+	t_head_2->value = copy_token_value(t_head_1->value);
+}
+
+void	fill_heredoc_append(t_token **token_1, t_token_2 **token_2)
+{
+	t_token		*t_head_1;
+	t_token_2	*t_head_2;
+
+	t_head_1 = *token_1;
+	t_head_2 = *token_2;
 	if (!token_1)
 		return ;
-	if (t_head_1->type && t_head_1->next->value
-		&& (ft_strcmp(t_head_1->type, "MOT") == 0))
+	if (t_head_1->type && (ft_strcmp(t_head_1->type, "APPEND") == 0))
 	{
-		if (prev)
-			prev->next = t_head_2->next;
-		if (!t_head_2->type)
-		{
-			t_head_2->type = "CMD";
-			t_head_2->value = copy_token_value(t_head_1->value);
-		}
-		if (t_head_2 && prev && ft_strcmp(prev->type, "MOT"))
-		{
-			t_head_2->type = "ARGS";
-			t_head_2->value = copy_token_value(t_head_1->next->value);
-		}
+		t_head_2->type = "APPEND";
+		t_head_2->value = copy_token_value(t_head_1->value);
+	}
+	else if (t_head_1->type && (ft_strcmp(t_head_1->type, "HEREDOC") == 0))
+	{
+		t_head_2->type = "HEREDOC";
+		t_head_2->value = copy_token_value(t_head_1->value);
 	}
 }
 
@@ -52,12 +69,17 @@ void	fill_redirin_redirout(t_token **token_1, t_token_2 **token_2)
 		return ;
 	if (t_head_1->type && (ft_strcmp(t_head_1->type, "REDIR_IN") == 0))
 	{
-		t_head_2->type = "INFILE";
+		t_head_2->type = "REDIR_IN";
 		t_head_2->value = copy_token_value(t_head_1->value);
 	}
 	else if (t_head_1->type && (ft_strcmp(t_head_1->type, "REDIR_OUT") == 0))
 	{
-		t_head_2->type = "OUTFILE";
+		t_head_2->type = "REDIR_OUT";
+		t_head_2->value = copy_token_value(t_head_1->value);
+	}
+	else if (t_head_1->type && (ft_strcmp(t_head_1->type, "PIPE") == 0))
+	{
+		t_head_2->type = "PIPE";
 		t_head_2->value = copy_token_value(t_head_1->value);
 	}
 }
