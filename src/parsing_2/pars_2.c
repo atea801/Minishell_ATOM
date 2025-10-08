75430605b2d615bc2ecd4cda12f9ec250e7d8946
aautret <aautret@student.42.fr>
2025-10-08 16:51:07 +0200

    PARS 2 : ajout fonctionnalite checker

--- src/parsing_2/pars_2.c
+++ src/parsing_2/pars_2.c
@@ -3,24 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   pars_2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 14:55:34 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/07 13:51:38 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/08 14:04:56 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
 
-void	parsing_2(t_token *token_head, t_token_2 *token_2)
+int	parsing_2(t_token *token_head, t_token_2 *token_2)
 {
-	t_token_2	*print;
 	t_token_2	*t_head_2;
-	t_token		*token_print;
+	t_token		*t_head_1;
 
-	token_print = token_head;
+	t_head_1 = token_head;
 	t_head_2 = token_2;
-	printf("nbr = %d\n", count_cmd(token_print));
-	print = get_input_pos(&token_head, &t_head_2);
-	return ;
+	tokenizer_2(t_head_1, t_head_2);
+	if (check_all(&t_head_2) > 0)
+		return (1);
+	return (0);
 }
