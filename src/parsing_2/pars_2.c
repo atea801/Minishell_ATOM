1224f56ac11bc2f1a2b34c9b3a44dee93a02f974
TOM <tomlorette2703@gmail.com>
2025-10-10 17:26:54 +0200

    PARS_2 : Demenagement de check pipe et check all

--- src/parsing_2/pars_2.c
+++ src/parsing_2/pars_2.c
@@ -3,14 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   pars_2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/05 14:55:34 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/09 17:25:18 by aautret          ###   ########.fr       */
+/*   Updated: 2025/10/10 17:22:45 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
+
+/**
+ * @brief Détecte si la liste commence ou finit par un pipe,
+ * ou si deux pipes se suivent, ce qui indique une erreur de syntaxe.
+ *
+ * @param token_2
+ * @return int
+ */
+int	check_pipe(t_token *token_2)
+{
+	t_token	*t_head_2;
+
+	t_head_2 = token_2;
+	if (!t_head_2 || !t_head_2->type)
+		return (0);
+	if (ft_strcmp(t_head_2->type, "PIPE") == 0)
+		return (1);
+	while (t_head_2->next && t_head_2->next->type)
+	{
+		if (ft_strcmp(t_head_2->type, "PIPE") == 0
+			&& ft_strcmp(t_head_2->next->type, "PIPE") == 0)
+			return (1);
+		t_head_2 = t_head_2->next;
+	}
+	if (ft_strcmp(t_head_2->type, "PIPE") == 0)
+		return (1);
+	return (0);
+}
 
 int	check_error(t_token *token_head)
 {
@@ -26,6 +54,29 @@ int	check_error(t_token *token_head)
 	return (0);
 }
 
+/**
+ * @brief Vérifie la validité globale de la liste de tokens.
+ *
+ * - Appelle les différents checkers pour valider la syntaxe des tokens
+ * @param token_2
+ * @return int
+ */
+int	check_all(t_token **token_head)
+{
+	t_token	*t_head_1;
+
+	if (!token_head || !*token_head)
+		return (0);
+	t_head_1 = *token_head;
+	if (check_pipe(t_head_1) > 0)
+		return (1);
+	else if ((parse_redir_alone(&t_head_1) > 0))
+		return (1);
+	else if (check_error(t_head_1) > 0)
+		return (1);
+	return (0);
+}
+
 int	parsing_2(t_token *token_head, t_token_2 *token_2)
 {
 	t_token_2	*t_head_2;
@@ -34,15 +85,11 @@ int	parsing_2(t_token *token_head, t_token_2 *token_2)
 
 	t_head_1 = token_head;
 	t_head_2 = token_2;
-	error = check_error(t_head_1);
+	error = check_all(&t_head_1);
 	if (error > 0)
 		return (1);
 	else if (error == 0)
-	{
 		tokenizer_2(t_head_1, t_head_2);
-		if (check_all(&t_head_2) > 0)
-			return (1);
-	}
 	return (0);
 }
 
