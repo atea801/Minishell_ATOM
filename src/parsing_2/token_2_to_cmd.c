a1a1568da71a01afc2246c99cf734e332490a787
TOM <tomlorette2703@gmail.com>
2025-10-16 17:12:55 +0200

    TOKEN_TO_CMD : grosse avancer permettant de transformer token_2 en double tableau pres a l exec

--- src/parsing_2/token_2_to_cmd.c
+++ src/parsing_2/token_2_to_cmd.c
@@ -6,93 +6,108 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 15:36:57 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/15 16:47:30 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/16 16:52:11 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
 
-void	token_2_to_cmd(t_token_2 **token_2, t_cmd **cmd)
+void	token_2_to_cmd(t_cmd **cmd, t_token_2 **token_2)
 {
 	t_token_2	*t_head_2;
-	t_cmd		*cmd_head;
-	int			count;
-	int			i;
+	t_cmd		*current_cmd;
+	t_cmd		*new_cmd;
 
-	i = 0;
 	t_head_2 = *token_2;
-	cmd_head = *cmd;
-	count = count_to_pipe(t_head_2);
+	*cmd = NULL;
 	while (t_head_2 && t_head_2->type)
 	{
-		if (ft_strcmp(t_head_2->type, "PIPE") != 0)
+		if (init_cmd_struct(&new_cmd) != 0)
+			return ;
+		if (*cmd == NULL)
+			*cmd = new_cmd;
+		else
 		{
-			fill_one_node(cmd_head, t_head_2, t_head_2->value, i);
-			fill_out_in_file(cmd_head, t_head_2, t_head_2->value);
-			set_heredoc_append(cmd_head, t_head_2);
-			i++;
+			current_cmd = *cmd;
+			while (current_cmd->next)
+				current_cmd = current_cmd->next;
+			current_cmd->next = new_cmd;
 		}
-		else if (ft_strcmp(t_head_2->type, "PIPE") == 0)
-		{
-			cmd_head = cmd_head->next;
-			i = 0;
-		}
-		t_head_2 = t_head_2->next;
+		fill_cmd_until_pipe(new_cmd, &t_head_2);
+		if (t_head_2 && t_head_2->type && ft_strcmp(t_head_2->type,
+				"PIPE") == 0)
+			t_head_2 = t_head_2->next;
 	}
+	print_cmd_list(*cmd);
 }
 
-void	fill_one_node(t_cmd *cmd_head, t_token_2 *t_head_2, char *str, int i)
-{
-	if (ft_strcmp(t_head_2->type, "CMD") == 0 || ft_strcmp(t_head_2->type,
-			"ARGS") == 0)
-	{
-		cmd_head->argv[i] = ft_strdup(str);
-		if (!cmd_head->argv[i])
-			return ;
-	}
-	else
-		return ;
-}
-
-void	fill_out_in_file(t_cmd *cmd_head, t_token_2 *t_head_2, char *str)
-{
-	if (ft_strcmp(t_head_2->type, "INFILE") == 0)
-	{
-		cmd_head->infile = ft_strdup(str);
-		if (!cmd_head->infile)
-			return ;
-	}
-	if (ft_strcmp(t_head_2->type, "OUTFILE") == 0)
-	{
-		cmd_head->outfile = ft_strdup(str);
-		if (!cmd_head->outfile)
-			return ;
-	}
-	else
-		return ;
-}
-
-void	set_heredoc_append(t_cmd *cmd_head, t_token_2 *t_head_2)
-{
-	if (ft_strcmp(t_head_2->type, "APPEND") == 0)
-		cmd_head->append = 1;
-	if (ft_strcmp(t_head_2->type, "HEREDOC") == 0)
-		cmd_head->here_doc = 1;
-	else
-		return ;
-}
-
-int	count_to_pipe(t_token_2 *t_head_2)
+void	fill_cmd_until_pipe(t_cmd *cmd, t_token_2 **t_head_2)
 {
 	int	count;
+	int	i;
+
+	count = count_args_to_pipe(*t_head_2);
+	cmd->argv = malloc(sizeof(char *) * (count + 1));
+	if (!cmd->argv)
+		return ;
+	i = 0;
+	while (*t_head_2 && (*t_head_2)->type && ft_strcmp((*t_head_2)->type,
+			"PIPE") != 0)
+	{
+		if (ft_strcmp((*t_head_2)->type, "CMD") == 0
+			|| ft_strcmp((*t_head_2)->type, "ARGS") == 0)
+		{
+			cmd->argv[i] = ft_strdup((*t_head_2)->value);
+			if (!cmd->argv[i])
+				return ;
+			i++;
+		}
+		*t_head_2 = (*t_head_2)->next;
+	}
+	cmd->argv[i] = NULL;
+	cmd->argc = i;
+}
+
+int	count_args_to_pipe(t_token_2 *token_2)
+{
+	int			count;
+	t_token_2	*t_head_2;
 
 	count = 0;
+	t_head_2 = token_2;
 	while (t_head_2 && t_head_2->type && ft_strcmp(t_head_2->type, "PIPE") != 0)
 	{
-		if (ft_strcmp(t_head_2->type, "CMD") == 0 || ft_strcmp(t_head_2->type,
-				"ARGS") == 0)
+		if (ft_strcmp(t_head_2->type, "ARGS") == 0 || ft_strcmp(t_head_2->type,
+				"CMD") == 0)
 			count++;
 		t_head_2 = t_head_2->next;
 	}
 	return (count);
+}
+
+void	print_cmd_list(t_cmd *cmd)
+{
+	int node = 0;
+	int i;
+
+	while (cmd)
+	{
+		printf("Node %d: ", node);
+		if (cmd->argv)
+		{
+			i = 0;
+			while (cmd->argv[i])
+			{
+				printf("%s ", cmd->argv[i]);
+				i++;
+			}
+		}
+		if (cmd->infile)
+			printf("< %s ", cmd->infile);
+		if (cmd->outfile)
+			printf("> %s ", cmd->outfile);
+		printf("\n");
+		cmd = cmd->next;
+		node++;
+	}
 }
