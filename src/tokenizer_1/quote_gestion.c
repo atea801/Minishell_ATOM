1180a9aeee943a7fe7911ea0a053f1288cff588e
TOM <tomlorette2703@gmail.com>
2025-10-17 18:04:17 +0200

    quotes gestion : mise a jour de la gestion des quotes

--- src/tokenizer_1/quote_gestion.c
+++ src/tokenizer_1/quote_gestion.c
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   quote_gestion.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/25 17:35:21 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/08 14:42:37 by aautret          ###   ########.fr       */
+/*   Updated: 2025/10/17 17:57:33 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
 
 /**
- * @brief Dettecte si l'element suivant une double quote suivant une simple quote /
+
+	* @brief Dettecte si l'element suivant une double quote suivant une simple quote
+	/
  * ou si l'element suivant une simple quote suivant une double quote
  *
  * - "' ou '" => return 1
@@ -29,11 +31,13 @@ int	quote_state(char c, char next)
 }
 
 /**
- * @brief Permet de trouver la fin d'une quote apres avoir 
+ * @brief Permet de trouver la fin d'une quote apres avoir
  * detecter le debut d'une quote
  *
- * - si le caractere a la position i est une double quote on avance jusqu'au prochain guillemet double
- * - si le caractere a la position i est une simple quote on avance jusqu'au prochain guillemet simple
+ *
+	- si le caractere a la position i est une double quote on avance jusqu'au prochain guillemet double
+ *
+	- si le caractere a la position i est une simple quote on avance jusqu'au prochain guillemet simple
  *
  * @param str
  * @param i
@@ -61,7 +65,8 @@ int	skip_quote(char *str, int i)
  le texte entre le quotes est copie mais pas les quotes exterieurs
  *
  *
- * == extraire dans les cas speciaux "' ou '" => on copie la sous chaine avec les quotes
+
+	* == extraire dans les cas speciaux "' ou '" => on copie la sous chaine avec les quotes
  * ("'hello'" => 'hello')
  * @param token
  * @param str
@@ -78,24 +83,30 @@ void	handle_quote_state(t_token **token, char *str, int *start, int end)
 }
 
 /**
- * @brief Permet d'isoler et de stocker le mot entre guillemets, on inclue pas les guillemets dans le token
+ * @brief Permet d'isoler et de stocker le mot entre guillemets,
+	on inclue pas les guillemets dans le token
  *
 
 
- * == extraire le texte situe a l'interieur des quotes simple ou double sans inclures les quotes dans le token
+
+	* == extraire le texte situe a l'interieur des quotes simple ou double sans inclures les quotes dans le token
  * @param token
  * @param str
  * @param start
  * @param end
  */
-void	handle_quote(t_token **token, char *str, int *start, int end)
+int	handle_quote(t_token **token, char *str, int *start, int end)
 {
 	char	*res;
 
 	res = malloc_token(end, *start + 1);
+	if (!res)
+		return (1);
 	copy_word(res, str, end, *start + 1);
-	put_token(token, res);
+	if (put_token(token, res))
+		return (free(res), 1);
 	(*start) += 1;
+	return (0);
 }
 
 /**
@@ -122,26 +133,30 @@ void	handle_general(t_token **token, char *str, int *start, int i)
  * - cas special "' ou '" == handle_quote_state
  * - cas mots entre quotes similaires simple ou double == handle_quote
  * - cas mots normaux == handle_general
- * 
+ *
  *
  * @param token
  * @param str
  * @param start
  * @param end
  */
-void	handle_all(t_token **token, char *str, int start, int i)
+int	handle_all(t_token **token, char *str, int start, int i)
 {
-	if ((str[start] == '"' && str[start + 1] == 39)
-		|| (str[start] == 39 && str[start + 1] == '"'))
+	if ((str[start] == '\"' && str[start + 1] == '\'') || (str[start] == '\''
+			&& str[start + 1] == '\"'))
 	{
 		start = start + 1;
 		handle_quote_state(token, str, &start, i - 1);
 	}
 	else
 	{
-		if (str[i] == '"' || str[i] == 39)
-			handle_quote(token, str, &start, i - 1);
+		if (str[i] == '\"' || str[i] == '\'')
+		{
+			if (handle_quote(token, str, &start, i - 1))
+				return (1);
+		}
 		else
 			handle_general(token, str, &start, i);
 	}
+	return (0);
 }
