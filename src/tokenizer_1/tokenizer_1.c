822bf5f81f5f3b73bd65cffffbc635af7b825eb6
TOM <tomlorette2703@gmail.com>
2025-10-17 18:00:35 +0200

    TOKENIZER_1 : modification d entrer dans la condition handle all pour gere les quotes coller

--- src/tokenizer_1/tokenizer_1.c
+++ src/tokenizer_1/tokenizer_1.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   token.c                                            :+:      :+:    :+:   */
+/*   tokenizer_1.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/22 16:39:47 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/10 18:52:38 by aautret          ###   ########.fr       */
+/*   Updated: 2025/10/17 17:55:11 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,17 +28,17 @@ void	tokenizer(t_token *token, char *str)
 	start = 0;
 	while (str[++i])
 	{
-		if (str[i] == '"' || str[i] == 39)
+		if (str[i] == '\"' || str[i] == '\'')
 		{
 			start = i;
 			i = skip_quote(str, i);
 		}
-		if (str[i + 1] == ' ' || str[i + 1] == 0)
+		if (str[i + 1] == ' ' || str[i + 1] == 0 || str[i + 1] == '\"' || str[i + 1] == '\'')
 		{
 			handle_all(&token, str, start, i);
 			start = i + 1;
 			while (str[start] && str[start] == ' ')
-				start++;
+			start++;
 		}
 	}
 }
