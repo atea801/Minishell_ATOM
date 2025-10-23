d6fde5c50e7a694ea89dbe374679812eb5b9de34
aautret <aautret@student.42.fr>
2025-10-23 12:11:09 +0200

    TOKEN 2 : modification token_2_to_cmd, separation fonction, gestion des leaks de outfile, infile, args

--- src/parsing_2/token_2_to_cmd.c
+++ src/parsing_2/token_2_to_cmd.c
@@ -5,12 +5,104 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/15 15:36:57 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/21 16:00:35 by aautret          ###   ########.fr       */
+/*   Created: 2025/10/23 11:58:02 by aautret           #+#    #+#             */
+/*   Updated: 2025/10/23 12:03:31 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "atom.h"
+
+int	count_args_to_pipe(t_token_2 *token_2)
+{
+	int			count;
+	t_token_2	*t_head_2;
+
+	count = 0;
+	t_head_2 = token_2;
+	while (t_head_2 && t_head_2->type && ft_strcmp(t_head_2->type, "PIPE") != 0)
+	{
+		if (ft_strcmp(t_head_2->type, "ARGS") == 0 || ft_strcmp(t_head_2->type,
+				"CMD") == 0)
+			count++;
+		t_head_2 = t_head_2->next;
+	}
+	return (count);
+}
+
+static int	allocate_argv(t_cmd *cmd, t_token_2 *t_head_2)
+{
+	int	count;
+
+	count = count_args_to_pipe(t_head_2);
+	cmd->argv = ft_calloc(count + 1, sizeof(char *));
+	if (!cmd->argv)
+		return (-1);
+	return (0);
+}
+
+static int	add_cmd_arg(t_cmd *cmd, t_token_2 *token, int index)
+{
+	if (ft_strcmp(token->type, "CMD") == 0 || ft_strcmp(token->type,
+			"ARGS") == 0)
+	{
+		if (cmd->argv[index])
+			free(cmd->argv[index]);
+		cmd->argv[index] = ft_strdup(token->value);
+		if (!cmd->argv[index])
+			return (-1);
+		return (1);
+	}
+	return (0);
+}
+
+static void	process_current_redirection(t_cmd *cmd, t_token_2 *token)
+{
+	if (ft_strcmp(token->type, "HEREDOC_DELIM") == 0)
+	{
+		if (cmd->heredoc_delim)
+			free(cmd->heredoc_delim);
+		cmd->heredoc_delim = ft_strdup(token->value);
+		cmd->here_doc = 1;
+	}
+	else if (ft_strcmp(token->type, "APPEND") == 0)
+		cmd->append = 1;
+	else if (ft_strcmp(token->type, "INFILE") == 0)
+	{
+		if (cmd->infile)
+			free(cmd->infile);
+		cmd->infile = ft_strdup(token->value);
+		cmd->here_doc = 0;
+	}
+	else if (ft_strcmp(token->type, "OUTFILE") == 0)
+	{
+		if (cmd->outfile)
+			free(cmd->outfile);
+		cmd->outfile = ft_strdup(token->value);
+		cmd->append = 0;
+	}
+}
+
+void	fill_cmd_until_pipe(t_cmd *cmd, t_token_2 **t_head_2)
+{
+	int	i;
+	int	result;
+
+	if (allocate_argv(cmd, *t_head_2) == -1)
+		return ;
+	i = 0;
+	while (*t_head_2 && (*t_head_2)->type && ft_strcmp((*t_head_2)->type,
+			"PIPE") != 0)
+	{
+		result = add_cmd_arg(cmd, *t_head_2, i);
+		if (result == -1)
+			return ;
+		i += result;
+		process_current_redirection(cmd, *t_head_2);
+		*t_head_2 = (*t_head_2)->next;
+	}
+	cmd->argv[i] = NULL;
+	cmd->argc = i;
+}
 
 void	token_2_to_cmd(t_cmd **cmd, t_token_2 **token_2)
 {
@@ -40,57 +132,12 @@ void	token_2_to_cmd(t_cmd **cmd, t_token_2 **token_2)
 	}
 }
 
-void	fill_cmd_until_pipe(t_cmd *cmd, t_token_2 **t_head_2)
-{
-	int	count;
-	int	i;
-
-	count = count_args_to_pipe(*t_head_2);
-	cmd->argv = malloc(sizeof(char *) * (count + 1));
-	if (!cmd->argv)
-		return ;
-	i = 0;
-	while (*t_head_2 && (*t_head_2)->type && ft_strcmp((*t_head_2)->type,
-			"PIPE") != 0)
-	{
-		if (ft_strcmp((*t_head_2)->type, "CMD") == 0
-			|| ft_strcmp((*t_head_2)->type, "ARGS") == 0)
-		{
-			cmd->argv[i] = ft_strdup((*t_head_2)->value);
-			if (!cmd->argv[i])
-				return ;
-			i++;
-		}
-		set_heredoc_delim_append(t_head_2, &cmd);
-		set_infile_cmd(t_head_2, &cmd);
-		*t_head_2 = (*t_head_2)->next;
-	}
-	cmd->argv[i] = NULL;
-	cmd->argc = i;
-}
-
-int	count_args_to_pipe(t_token_2 *token_2)
-{
-	int			count;
-	t_token_2	*t_head_2;
-
-	count = 0;
-	t_head_2 = token_2;
-	while (t_head_2 && t_head_2->type && ft_strcmp(t_head_2->type, "PIPE") != 0)
-	{
-		if (ft_strcmp(t_head_2->type, "ARGS") == 0 || ft_strcmp(t_head_2->type,
-				"CMD") == 0)
-			count++;
-		t_head_2 = t_head_2->next;
-	}
-	return (count);
-}
-
 void	print_cmd_list(t_cmd *cmd)
 {
-	int node = 0;
+	int node;
 	int i;
 
+	node = 0;
 	while (cmd)
 	{
 		printf("\nCOMMANDE %d\n", node);
@@ -111,48 +158,5 @@ void	print_cmd_list(t_cmd *cmd)
 		printf("APPEND : %d\n", cmd->append);
 		cmd = cmd->next;
 		node++;
-	}
-}
-
-void	set_heredoc_delim_append(t_token_2 **token_2, t_cmd **cmd)
-{
-	t_cmd		*cmd_head;
-	t_token_2	*t_head_2;
-
-	cmd_head = *cmd;
-	t_head_2 = *token_2;
-	while (t_head_2 && t_head_2->type && ft_strcmp(t_head_2->type, "PIPE") != 0)
-	{
-		if (ft_strcmp(t_head_2->type, "HEREDOC_DELIM") == 0)
-		{
-			cmd_head->heredoc_delim = ft_strdup(t_head_2->value);
-			cmd_head->here_doc = 1;
-		}
-		if (ft_strcmp(t_head_2->type, "APPEND") == 0)
-			cmd_head->append = 1;
-		t_head_2 = t_head_2->next;
-	}
-}
-
-void	set_infile_cmd(t_token_2 **token_2, t_cmd **cmd)
-{
-	t_cmd		*cmd_head;
-	t_token_2	*t_head_2;
-
-	cmd_head = *cmd;
-	t_head_2 = *token_2;
-	while (t_head_2 && t_head_2->type && ft_strcmp(t_head_2->type, "PIPE") != 0)
-	{
-		if (ft_strcmp(t_head_2->type, "INFILE") == 0)
-		{
-			cmd_head->infile = ft_strdup(t_head_2->value);
-			cmd_head->here_doc = 0;
-		}
-		if (ft_strcmp(t_head_2->type, "OUTFILE") == 0)
-		{
-			cmd_head->outfile = ft_strdup(t_head_2->value);
-			cmd_head->append = 0;
-		}
-		t_head_2 = t_head_2->next;
 	}
 }
