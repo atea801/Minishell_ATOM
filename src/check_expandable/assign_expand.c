e8742302c51ef7e05b69ce2886c084ee04d264e4
TOM <tomlorette2703@gmail.com>
2025-10-28 18:18:32 +0100

    EXPANDER : debut de travail sur les multiple expand

--- src/check_expandable/assign_expand.c
+++ src/check_expandable/assign_expand.c
@@ -6,7 +6,7 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 11:45:18 by tlorette          #+#    #+#             */
-/*   Updated: 2025/10/23 18:54:50 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/28 18:15:08 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,24 +41,73 @@ int	special_expand(t_minishell *shell, t_token_2 *token_2)
 	return (0);
 }
 
-int	expander(t_minishell *shell, t_token_2 *token_2)
+int	get_expand(t_cmd *cmd)
+{
+	int		j;
+	int		i;
+	int		key_len;
+	char	*key;
+
+	j = 0;
+	i = 0;
+	while (cmd->argv[i])
+	{
+		if (cmd->argv[i][0] == '$')
+			return (i);
+		i++;
+	}
+	return (0);
+}
+
+char	*multiple_expand(t_minishell *shell, t_cmd *cmd)
+{
+	int		i;
+	int		j;
+	char	*key;
+	char	*res;
+	int		key_len;
+
+	i = get_expand(cmd);
+	j = 0;
+	while (cmd->argv[i][++j])
+	{
+		if (cmd->argv[i][j] == '$')
+		{
+			key_len = j;
+			key = malloc(sizeof(char) * j + 1);
+			j = 0;
+			while (j < key_len)
+			{
+				key[j] = cmd->argv[i][j];
+				j++;
+			}
+			res = ft_strdup(search_in_list(&shell->env, key));
+			free(key);
+		}
+	}
+	return (res);
+}
+
+int	expander(t_minishell *shell, t_cmd *cmd)
 {
 	char	*key;
 	char	*expanded;
+	int		i;
 
-	if (!token_2 || !token_2->value)
+	if (!cmd || !cmd->argv)
 		return (0);
-	if (special_expand(shell, token_2) == 1)
+	if (special_expand(shell, cmd) == 1)
 		return (1);
-	key = get_key(token_2->value);
-	if (!key)
-		return (0);
+	i = 0;
+	// key = get_key(token_2->value);
+	// if (!key)
+	// 	return (0);
 	expanded = search_in_list(&shell->env, key);
 	free(key);
 	if (expanded)
 	{
-		free(token_2->value);
-		token_2->value = ft_strdup(expanded);
+		free(cmd->argv[i]);
+		cmd->argv[i] = ft_strdup(expanded);
 	}
 	return (1);
 }
--- src/check_expandable/check_expand.c
+++ src/check_expandable/check_expand.c
@@ -6,7 +6,7 @@
 /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 19:27:38 by aautret           #+#    #+#             */
-/*   Updated: 2025/10/23 18:54:01 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/10/28 15:25:42 by tlorette         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@ int	in_double_quote(char *res, int pos)
 	return (in_double);
 }
 
+
 void	check_expendable(char *res, t_token_2 *token_2)
 {
 	int		index;
@@ -58,14 +59,15 @@ void	check_expendable(char *res, t_token_2 *token_2)
 		{
 			index = pos - res;
 			if ((!in_single_quote(res, index) && !in_double_quote(res, index))
-				|| (in_double_quote(res, index) && !in_single_quote(res, index)))
+				|| (in_double_quote(res, index) && !in_single_quote(res,
+						index)))
 				token_2->is_expand = 1;
 			else if (in_single_quote(res, index))
 				token_2->is_expand = 0;
 			else
 				token_2->is_expand = 0;
 		}
-		// printf("%s is_expand = %d\n", token_2->value, token_2->is_expand);
 		token_2 = token_2->next;
 	}
 }
+
