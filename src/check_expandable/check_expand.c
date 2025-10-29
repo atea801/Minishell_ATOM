d8eb7effcd41b49d6dabfe9227ad99bc65805827
TOM <tomlorette2703@gmail.com>
2025-10-29 16:30:56 +0100

    expand fini

--- src/check_expandable/check_expand.c
+++ src/check_expandable/check_expand.c
@@ -1,73 +1,99 @@
-// /* ************************************************************************** */
-// /*                                                                            */
-// /*                                                        :::      ::::::::   */
-// /*   check_expand.c                                     :+:      :+:    :+:   */
-// /*                                                    +:+ +:+         +:+     */
-// /*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
-// /*                                                +#+#+#+#+#+   +#+           */
-// /*   Created: 2025/10/10 19:27:38 by aautret           #+#    #+#             */
-// /*   Updated: 2025/10/28 15:25:42 by tlorette         ###   ########.fr       */
-// /*                                                                            */
-// /* ************************************************************************** */
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_expand.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/10 19:27:38 by aautret           #+#    #+#             */
+/*   Updated: 2025/10/29 16:40:20 by tlorette         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 // #include "atom.h"
 
-// int	in_single_quote(char *res, int pos)
-// {
-// 	int	in_single;
-// 	int	i;
+static int	var_len(char *s, int start)
+{
+	int	i;
 
-// 	in_single = 0;
-// 	i = 0;
-// 	while (i < pos)
-// 	{
-// 		if (res[i] == '\'')
-// 			in_single = !in_single;
-// 		i++;
-// 	}
-// 	return (in_single);
-// }
+	i = 0;
+	if (!s)
+		return (0);
+	while (ft_isalnum(s[start + i]) || s[start + i] == '_')
+		i++;
+	return (i);
+}
 
-// int	in_double_quote(char *res, int pos)
-// {
-// 	int	in_double;
-// 	int	i;
+static char	*ft_strjoin_free(char *s1, char *s2)
+{
+	char	*res;
 
-// 	in_double = 0;
-// 	i = 0;
-// 	while (i < pos)
-// 	{
-// 		if (res[i] == '\"')
-// 			in_double = !in_double;
-// 		i++;
-// 	}
-// 	return (in_double);
-// }
+	res = ft_strjoin(s1, s2);
+	if (!res)
+		return (NULL);
+	free(s1);
+	free(s2);
+	return (res);
+}
 
+static char	*process_variable(t_minishell *shell, char *s, int *i, char *res)
+{
+	char	*before;
+	int		len;
 
-// void	check_expendable(char *res, t_token_2 *token_2)
-// {
-// 	int		index;
-// 	char	*pos;
+	before = ft_substr(s, 0, *i);
+	res = ft_strjoin_free(res, before);
+	if (!res)
+		return (NULL);
+	len = 1 + var_len(s, *i + 1);
+	if (s[*i + 1] == '?')
+		len = 2;
+	before = ft_substr(s, *i, len);
+	if (!before)
+		return (free(res), NULL);
+	res = ft_strjoin_free(res, replace_env_var(shell, before));
+	return (res);
+}
 
-// 	index = 0;
-// 	while (token_2 && token_2->value)
-// 	{
-// 		if (res)
-// 			pos = ft_strnstr(res + index, token_2->value, ft_strlen(res));
-// 		if (pos && token_2->value[0] == '$')
-// 		{
-// 			index = pos - res;
-// 			if ((!in_single_quote(res, index) && !in_double_quote(res, index))
-// 				|| (in_double_quote(res, index) && !in_single_quote(res,
-// 						index)))
-// 				token_2->is_expand = 1;
-// 			else if (in_single_quote(res, index))
-// 				token_2->is_expand = 0;
-// 			else
-// 				token_2->is_expand = 0;
-// 		}
-// 		token_2 = token_2->next;
-// 	}
-// }
+static char	*handle_dollar(t_minishell *shell, char **s, int *i, char *res)
+{
+	int	len;
 
+	len = 1 + var_len(*s, *i + 1);
+	if ((*s)[*i + 1] == '?')
+		len = 2;
+	res = process_variable(shell, *s, i, res);
+	if (!res)
+		return (NULL);
+	*s += *i + len;
+	*i = 0;
+	return (res);
+}
+
+void	expand_all_vars(t_minishell *shell, t_token_2 *token)
+{
+	char	*res;
+	char	*s;
+	int		i;
+
+	res = ft_strdup("");
+	if (!res)
+		return ;
+	s = token->value;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '$')
+		{
+			res = handle_dollar(shell, &s, &i, res);
+			if (!res)
+				return (free(token->value), (void)0);
+		}
+		else
+			i++;
+	}
+	if (*s)
+		res = ft_strjoin_free(res, ft_strdup(s));
+	free(token->value);
+	token->value = res;
+}
