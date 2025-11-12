f42f14a0fcac19c9eedcf014796645ce9a6104d0
aautret <aautret@student.42.fr>
2025-11-12 17:40:58 +0100

    EXPORT : implementaiton et correction

--- src/built_in/export.c
+++ src/built_in/export.c
@@ -6,7 +6,7 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 15:06:05 by aautret           #+#    #+#             */
-/*   Updated: 2025/11/12 16:34:16 by aautret          ###   ########.fr       */
+/*   Updated: 2025/11/12 17:38:07 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,41 +22,36 @@
 //      sinon marque exportee sans changer la valeur
 
 
-static int	check_args_export(char *str)
-{
-	int	i;
+// static int	check_args_export(char *str)
+// {
+// 	int	i;
 
-	if (!str || str[0] == '\0')
-		return (1);
-	i = 1;
-	if (str[0] == '_' || ft_isalpha((unsigned char)str[0]))
-	{
-		while (str[i])
-		{
-			if (ft_isalnum((unsigned char)str[i]) || str[i] == '_')
-				i++;
-			else
-				return (1);
-		}
-		return (0);
-	}
-	return (1);
-}
+// 	if (!str || str[0] == '\0')
+// 		return (1);
+// 	i = 1;
+// 	if (str[0] == '_' || ft_isalpha((unsigned char)str[0]))
+// 	{
+// 		while (str[i])
+// 		{
+// 			if (ft_isalnum((unsigned char)str[i]) || str[i] == '_')
+// 				i++;
+// 			else
+// 				return (1);
+// 		}
+// 		return (0);
+// 	}
+// 	return (1);
+// }
 
 int	builtin_export(t_minishell *shell)
 {
-	if (shell->cmd->argv[1] && check_args_export(shell->cmd->argv[1]) == 0)
+	if (shell->cmd->argc > 1 && shell->cmd->argv[1])
 	{
 		valide_key(&shell->env, shell->cmd->argv[1]);
 		printf("Success add\n");
 		shell->should_exit = 0;
+		return (0);
 	}
-	else
-	{
-		printf("Nothing exported\n");
-		shell->should_exit = 1;
-		return (1);
-	}
-	return (0);
+	return (1);
 }
 
