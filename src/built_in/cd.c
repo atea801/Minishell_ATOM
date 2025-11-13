512c6e5cf20b04d2a9239ff4bc43e922b82d2ef1
aautret <aautret@student.42.fr>
2025-11-13 16:14:52 +0100

    CD : norme

--- src/built_in/cd.c
+++ src/built_in/cd.c
@@ -6,7 +6,7 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/31 14:35:15 by aautret           #+#    #+#             */
-/*   Updated: 2025/11/09 11:50:48 by aautret          ###   ########.fr       */
+/*   Updated: 2025/11/13 14:57:42 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,25 @@ int	cd_init_vars(t_minishell *shell, char **home, char **oldpwd)
  * 
  * - gestion de cd ~
  * 
- * - gestion de cd (avec "" + "    ") partie en commentaire
+ * - gestion de cd (avec "" + "    ") partie en commentaire 
+ * 
+ * 
+ * // // Vérifier si l'argument est vide ou ne contient que des espaces
+ * // if (!shell->cmd->argv[1] || shell->cmd->argv[1][0] == '\0')
+ * // {
+ * // ft_putstr_fd("Minishell: cd: invalid argument\n", 2);
+ * // 	free(oldpwd);
+ * // 	return (1);
+ * // }
+ * // // Vérifier si l'argument ne contient que des espaces
+ * // i = 0;
+ * // while (shell->cmd->argv[1][i] && (shell->cmd->argv[1][i] == ' '
+ * // 		|| shell->cmd->argv[1][i] == '\t'
+ * // 		|| shell->cmd->argv[1][i] == '\n'
+ * // 		|| shell->cmd->argv[1][i] == '\r'
+ * // 		|| shell->cmd->argv[1][i] == '\v'
+ * // 		|| shell->cmd->argv[1][i] == '\f'))
+ * // 	i++;
  * @param shell 
  * @param home 
  * @param oldpwd 
@@ -65,22 +83,6 @@ int	cd_dispatch_case(t_minishell *shell, char *home, char *oldpwd,
 		return (case_cd_sin_arg(shell, home, oldpwd, new_pwd));
 	if (shell->cmd->argc == 2)
 	{
-		// // Vérifier si l'argument est vide ou ne contient que des espaces
-		// if (!shell->cmd->argv[1] || shell->cmd->argv[1][0] == '\0')
-		// {
-		// 	ft_putstr_fd("Minishell: cd: invalid argument\n", 2);
-		// 	free(oldpwd);
-		// 	return (1);
-		// }
-		// // Vérifier si l'argument ne contient que des espaces
-		// i = 0;
-		// while (shell->cmd->argv[1][i] && (shell->cmd->argv[1][i] == ' '
-		// 		|| shell->cmd->argv[1][i] == '\t'
-		// 		|| shell->cmd->argv[1][i] == '\n'
-		// 		|| shell->cmd->argv[1][i] == '\r'
-		// 		|| shell->cmd->argv[1][i] == '\v'
-		// 		|| shell->cmd->argv[1][i] == '\f'))
-		// 	i++;
 		if (!shell->cmd->argv[1][i])
 		{
 			ft_putstr_fd("Minishell: cd: invalid argument\n", 2);
