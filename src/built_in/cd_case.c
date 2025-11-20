c2d4ff5085bccdf005c94e6f61c218290aa8e794
aautret <aautret@student.42.fr>
2025-11-20 11:24:08 +0100

    CD : resolution conflits

--- src/built_in/cd_case.c
+++ src/built_in/cd_case.c
@@ -6,7 +6,7 @@
 /*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 15:30:31 by aautret           #+#    #+#             */
-/*   Updated: 2025/11/20 11:14:43 by aautret          ###   ########.fr       */
+/*   Updated: 2025/11/20 11:23:51 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -115,20 +115,20 @@ int	cd_special_case_dash(t_minishell *shell, char *old_pwd, char *new_pwd)
  * @param path 
  * @return int 0 = succes / 1 = erreur
  */
-static int	cd_with_args_check_access(char *path)
-{
-	if (access(path, F_OK) != 0)
-	{
-		cd_with_args_error_print(path);
-		return (1);
-	}
-	if (access(path, X_OK) != 0)
-	{
-		perror("Minishell: cd");
-		return (1);
-	}
-	return (0);
-}
+// static int	cd_with_args_check_access(char *path)
+// {
+// 	if (access(path, F_OK) != 0)
+// 	{
+// 		cd_with_args_error_print(path);
+// 		return (1);
+// 	}
+// 	if (access(path, X_OK) != 0)
+// 	{
+// 		perror("Minishell: cd");
+// 		return (1);
+// 	}
+// 	return (0);
+// }
 
 /**
  * @brief Gere le cas cd <path> quand un argument simple est donne
@@ -149,11 +149,11 @@ int	cd_with_args(t_minishell *shell, char *old_pwd, char *new_pwd)
 	char	*path;
 
 	path = ft_strdup(shell->cmd->argv[1]);
-	if (cd_with_args_check_access(path) > 0)
-	{
-		cd_with_args_free(old_pwd, path);
-		return (1);
-	}
+	// if (cd_with_args_check_access(path) > 0)
+	// {
+	// 	cd_with_args_free(old_pwd, path);
+	// 	return (1);
+	// }
 	if (chdir(path) != 0)
 	{
 		ft_putstr_fd("Minishell : cd :", 2);
