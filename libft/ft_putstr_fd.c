4a8e1a91aea521c613755d29cbe25504a8be3475
aautret <aautret@student.42.fr>
2025-11-24 18:34:24 +0100

    LIBFT : correction ft_putsrt_fd

--- libft/ft_putstr_fd.c
+++ libft/ft_putstr_fd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tlorette <tlorette@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aautret <aautret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/01 14:09:07 by tlorette          #+#    #+#             */
-/*   Updated: 2025/05/01 14:20:54 by tlorette         ###   ########.fr       */
+/*   Updated: 2025/11/24 18:24:03 by aautret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,7 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	int	i;
-
-	i = 0;
-	while (s[i])
-	{
-		write(fd, &s[i], 1);
-		i++;
-	}
+	write(fd, s, ft_strlen(s));
 }
 // int	main()
 // {
